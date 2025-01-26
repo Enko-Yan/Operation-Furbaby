a3924b8d36223254e54e0bd131adb82dbd49be20
Enk0-A <sergiyenkoyan@gmail.com>
2025-01-25 21:18:21 -0600

    Added a confirmation window that reads info back to user.

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -41,16 +41,53 @@ document.getElementById("applicationForm").addEventListener("submit", function(e
 
 
     
-    showModal();
+    showConfirmationModal();
+});
+
+function showConfirmationModal () {
+    const firstname = document.getElementById("first-name").value;
+    const lastname = document.getElementById("last-name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const address = document.getElementById("address").value;
+    const state = document.getElementById("state").value;
+    const interest = document.getElementById("interest").value;
+    
+    document.getElementById("confirm-first-name").textContent = firstname;
+    document.getElementById("confirm-last-name").textContent = lastname;
+    document.getElementById("confirm-email").textContent = email;
+    document.getElementById("confirm-phone").textContent = phone;
+    document.getElementById("confirm-address").textContent = address;
+    document.getElementById("confirm-state").textContent = state;
+    document.getElementById("confirm-interest").textContent = interest;
+
+    const confirmationModal = document.getElementById("confirmationModal");
+    confirmationModal.style.display = "block";
+}
+
+document.getElementById("confirmSubmit").addEventListener("click", function () {
+    const confirmationModal = document.getElementById("confirmationModal");
+    confirmationModal.style.display = "none";
+
+    const successModal = document.getElementById("modal");
+    successModal.style.display = "block";
+
+});
+
+document.getElementById("cancel").addEventListener("click", function () {
+    const confirmationModal = document.getElementById("confirmationModal");
+    confirmationModal.style.display = "none";
+});
+
+document.getElementById("closeConfirmationModal").addEventListener("click", function () {
+    const confirmationModal = document.getElementById("confirmationModal");
+    confirmationModal.style.display = "none";
 });
 
 function showModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "block";
 }
-
-
-
 
 document.getElementById("closeModal").addEventListener("click", function() {
     const modal = document.getElementById("modal");
